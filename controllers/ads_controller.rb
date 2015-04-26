4807c844e1e28789e0cc0c149923d8030a5bd1a5
pmollet <patrick.mollet@metaxis.ch>
2015-04-26 18:59:47 +0200

    Page 139: ad_params in AdsController (instead of params[:id]

--- controllers/ads_controller.rb
+++ controllers/ads_controller.rb
@@ -13,6 +13,14 @@ class AdsController < ApplicationController
   def show
     @ad = Ad.find(params[:id])
   end
+  def edit
+    @ad = Ad.find(params[:id])
+  end
+  def update
+    @ad = Ad.find(params[:id])
+    @ad.update_attributes ad_params
+    redirect_to "/ads/#{@ad.id}"
+  end
 
 private
 
